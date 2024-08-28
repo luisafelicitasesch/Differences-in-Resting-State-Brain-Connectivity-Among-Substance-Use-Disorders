0a44d62f3ff918c4f72280045425141f71492e00
luisafelicitasesch <luisa@grebin.net>
2024-08-28 13:17:17 +0200

    Update Script4_permuanova_residuals.m

--- SUDMEXCONN/Script4_permuanova_residuals.m
+++ SUDMEXCONN/Script4_permuanova_residuals.m
@@ -1,7 +1,7 @@
 % Setup paths
-path_BCT = 'C:\Users\luisa\MATLAB\packages\BCT';
-path_GraphVar = 'C:\Users\luisa\MATLAB\packages\GraphVar_2.03a';
-path_ComAlg = 'C:\Users\luisa\MATLAB\packages\communityalg-master';
+path_BCT = % add path to toolbox;
+path_GraphVar = % add path to toolbox;
+path_ComAlg = % add path to toolbox;
 addpath(genpath(path_BCT));
 addpath(genpath(path_GraphVar));
 addpath(genpath(path_ComAlg));
